d6773cb2dff1514053dec2f7e4c45cb8026a32db
何冠峰 <hevinci@hotmail.com>
2019-11-19 20:23:05 +0800

    Update DebugBundle.cs
    show error label when asset loader load failed.

--- Assets/MotionGame/Runtime/Game.Debug/DebugBundle.cs
+++ Assets/MotionGame/Runtime/Game.Debug/DebugBundle.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using MotionEngine.Res;
 using MotionEngine.Debug;
+using MotionEngine.Reference;
 using UnityEngine;
 
 namespace MotionGame
@@ -10,11 +11,31 @@ namespace MotionGame
 	[DebugAttribute("资源列表", 103)]
 	public class DebugBundle : IDebug
 	{
+		private class InfoWrapper : IReference , IComparer<InfoWrapper>, IComparable<InfoWrapper>
+		{
+			public string Info;
+			public EAssetFileLoadState LoadState;
+
+			public void OnRelease()
+			{
+				Info = string.Empty;
+				LoadState = EAssetFileLoadState.None;
+			}
+			public int CompareTo(InfoWrapper other)
+			{
+				return Compare(this, other);
+			}
+			public int Compare(InfoWrapper a, InfoWrapper b)
+			{
+				return string.CompareOrdinal(a.Info, b.Info);
+			}
+		}
+
 		// 加载器总数
 		private int _loaderTotalCount = 0;
 
 		// 显示信息集合
-		private List<string> _cacheInfos = new List<string>(1000);
+		private List<InfoWrapper> _cacheInfos = new List<InfoWrapper>(1000);
 
 		// 过滤的关键字
 		private string _filterKey = string.Empty;
@@ -39,11 +60,18 @@ namespace MotionGame
 			DebugConsole.GUILable($"加载器总数：{_loaderTotalCount}");
 			for (int i = 0; i < _cacheInfos.Count; i++)
 			{
-				DebugConsole.GUILable(_cacheInfos[i]);
+				var element = _cacheInfos[i];
+				if (element.LoadState == EAssetFileLoadState.LoadAssetFileFailed)
+					DebugConsole.GUIRedLable(element.Info);
+				else
+					DebugConsole.GUILable(element.Info);
 			}
 		}
 		private void FilterInfos()
 		{
+			// 回收引用
+			ReferenceSystem.Release(_cacheInfos);
+
 			// 清空列表
 			_cacheInfos.Clear();
 
@@ -59,16 +87,20 @@ namespace MotionGame
 						continue;
 				}
 
-				string showInfo = GameUtility.Substring(loader.LoadPath, "/assets/", false);
-				showInfo = showInfo.Replace(".unity3d", string.Empty);
-				showInfo = $"{showInfo} = {loader.RefCount}";
+				string info = GameUtility.Substring(loader.LoadPath, "/assets/", false);
+				info = info.Replace(".unity3d", string.Empty);
+				info = $"{info} = {loader.RefCount}";
+
+				InfoWrapper element = ReferenceSystem.Spawn<InfoWrapper>();
+				element.Info = info;
+				element.LoadState = loader.LoadState;
 
 				// 添加到显示列表
-				_cacheInfos.Add(showInfo);
+				_cacheInfos.Add(element);
 			}
 
 			// 重新排序
-			_cacheInfos.Sort(string.CompareOrdinal);
+			_cacheInfos.Sort();
 		}
 	}
 }
