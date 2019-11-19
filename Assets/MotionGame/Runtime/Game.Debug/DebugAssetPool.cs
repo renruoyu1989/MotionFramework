b3a5a5c1c554c298f6c8e30ee643c256c82c3a0d
何冠峰 <hevinci@hotmail.com>
2019-11-19 18:13:22 +0800

    Add debug asset pool node

---
+++ Assets/MotionGame/Runtime/Game.Debug/DebugAssetPool.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using MotionEngine;
+using MotionEngine.Debug;
+
+namespace MotionGame
+{
+	[DebugAttribute("实体资源池", 105)]
+	public class DebugPool : IDebug
+	{
+		public void OnInit()
+		{
+		}
+		public void OnGUI()
+		{
+			var pools = PoolManager.Instance.DebugAllPools;
+			DebugConsole.GUILable($"池总数：{pools.Count}");
+			foreach (var pair in pools)
+			{
+				DebugConsole.GUILable($"[{pair.Value.ResName}] CacheCount = {pair.Value.Count} SpwanCount = {pair.Value.SpawnCount}");
+			}
+		}
+	}
+}
