49c1032cb0ee3c1c0d438be7757bb16c6e2d4845
何冠峰 <hevinci@hotmail.com>
2019-11-25 17:09:59 +0800

    Update ParticleProfilerWindow
    can specify effects test scene.

--- Assets/MotionEngine/Editor/ParticleProfiler/ParticleProfilerWindow.cs
+++ Assets/MotionEngine/Editor/ParticleProfiler/ParticleProfilerWindow.cs
@@ -6,6 +6,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using UnityEditor.SceneManagement;
 
 public class ParticleProfilerWindow : EditorWindow
 {
@@ -29,6 +30,11 @@ public class ParticleProfilerWindow : EditorWindow
 	private UnityEngine.Object _effectPrefab = null;
 
 	/// <summary>
+	/// 预览所使用的场景
+	/// </summary>
+	private UnityEditor.SceneAsset _profilerScene = null;
+
+	/// <summary>
 	/// 粒子测试类
 	/// </summary>
 	private ParticleTester _tester = new ParticleTester();
@@ -45,27 +51,41 @@ public class ParticleProfilerWindow : EditorWindow
 	private bool _isShowTips = false;
 	private Texture2D _texTips = null;
 
+	private const string PROFILER_SCENE_KEY = "ParticleProfilerWindow_ScenePath";
+
+
 	private void Awake()
 	{
 		_lastTime = EditorApplication.timeSinceStartup;
 		_texTips = AssetDatabase.LoadAssetAtPath<Texture2D>("Assets/MotionEngine/Editor/ParticleProfiler/GUI/tips.png");
 		if (_texTips == null)
 			Debug.LogWarning("Not found ParticleProfilerWindows tips texture.");
+
+		string path = EditorPrefs.GetString(PROFILER_SCENE_KEY, string.Empty);
+		if(string.IsNullOrEmpty(path) == false)
+			_profilerScene = AssetDatabase.LoadAssetAtPath<SceneAsset>(path);
 	}
 	private void OnGUI()
 	{
 		EditorGUILayout.Space();
 
+		// 测试场景
+		SceneAsset scene = (SceneAsset)EditorGUILayout.ObjectField($"测试场景", _profilerScene, typeof(SceneAsset), false);
+		if(_profilerScene != scene)
+		{
+			_profilerScene = scene;
+			string path = AssetDatabase.GetAssetPath(scene);
+			EditorPrefs.SetString(PROFILER_SCENE_KEY, path);
+		}
+
+		// 测试特效
 		_effectPrefab = EditorGUILayout.ObjectField($"请选择特效", _effectPrefab, typeof(UnityEngine.Object), false);
 
 		// 测试按钮
 		if (GUILayout.Button("测试"))
 		{
-			if (_effectPrefab == null)
-			{
-				Debug.LogWarning("需要一个特效预制体！");
+			if (CheckProfilerCondition() == false)
 				return;
-			}
 
 			// 焦点锁定游戏窗口
 			var gameViewType = typeof(Editor).Assembly.GetType("UnityEditor.GameView");
@@ -104,7 +124,7 @@ public class ParticleProfilerWindow : EditorWindow
 		EditorGUILayout.LabelField($"三角面数：{_tester.TriangleCurrentCount}  最大：{_tester.TriangleMaxCount}");
 
 		// 错误信息
-		if(_tester.Errors.Count > 0)
+		if (_tester.Errors.Count > 0)
 		{
 			EditorGUILayout.Space();
 			EditorGUILayout.HelpBox($"请修正以下错误提示", MessageType.Error, true);
@@ -185,7 +205,7 @@ public class ParticleProfilerWindow : EditorWindow
 			_isShowTips = EditorGUILayout.Foldout(_isShowTips, "过程化检测结果");
 			if (_isShowTips)
 			{
-				EditorGUI.indentLevel = 1;	
+				EditorGUI.indentLevel = 1;
 				_scrollPos3 = EditorGUILayout.BeginScrollView(_scrollPos3);
 				{
 					GUILayout.Button(_texTips); //绘制提示图片
@@ -221,5 +241,41 @@ public class ParticleProfilerWindow : EditorWindow
 
 		// 刷新窗口界面
 		this.Repaint();
+	}
+
+	/// <summary>
+	/// 检测特效测试条件
+	/// </summary>
+	/// <returns>如果通过返回TRUE</returns>
+	private bool CheckProfilerCondition()
+	{
+		if (_effectPrefab == null)
+		{
+			Debug.LogWarning("请指定一个要测试的特效预制体！");
+			return false;
+		}
+
+		if (_profilerScene == null)
+		{
+			Debug.LogWarning("建议设置一个专门的特效场景测试！");
+			return true;
+		}
+
+		UnityEngine.SceneManagement.Scene activeScene = UnityEngine.SceneManagement.SceneManager.GetActiveScene();
+		if (activeScene.name == _profilerScene.name)
+			return true;
+
+		// 注意：如果当前场景未保存
+		if (activeScene.isDirty)
+		{
+			EditorUtility.DisplayDialog("警告", $"请先保存当前场景：{activeScene.name}", "确定");
+			return false;
+		}
+
+		// 打开特效测试场景
+		string path = AssetDatabase.GetAssetPath(_profilerScene);
+		Debug.Log($"打开特效测试场景：{path}");
+		EditorSceneManager.OpenScene(path);
+		return true;
 	}
 }
