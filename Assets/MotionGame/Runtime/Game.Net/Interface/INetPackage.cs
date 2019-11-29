8e72e611a09f484c7af5f9924235336cab0e9ba4
何冠峰 <hevinci@hotmail.com>
2019-11-29 10:52:18 +0800

    Add INetPackage interface

---
+++ Assets/MotionGame/Runtime/Game.Net/Interface/INetPackage.cs
@@ -0,0 +1,26 @@
+﻿
+namespace MotionGame
+{
+	public interface INetPackage
+	{
+		/// <summary>
+		/// 消息ID
+		/// </summary>
+		ushort MsgID { set; get; }
+
+		/// <summary>
+		/// 消息体
+		/// </summary>
+		System.Object MsgObj { set; get; }
+
+		/// <summary>
+		/// 字节数据
+		/// </summary>
+		byte[] MsgBytes { set; get; }
+
+		/// <summary>
+		/// 是否是Mono消息，否则为热更消息
+		/// </summary>
+		bool IsMonoPackage { set; get; }
+	}
+}
