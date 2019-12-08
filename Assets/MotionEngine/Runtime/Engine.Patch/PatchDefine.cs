e9c49566f9ed3d5990006ba414b059a7195b40f3
何冠峰 <hevinci@hotmail.com>
2019-12-08 19:24:25 +0800

    Update PatchDefine.cs

--- Assets/MotionEngine/Runtime/Engine.Patch/PatchDefine.cs
+++ Assets/MotionEngine/Runtime/Engine.Patch/PatchDefine.cs
@@ -10,21 +10,5 @@ namespace MotionEngine.Patch
 		public const string StrBundleSuffixName = ".unity3d"; // AssetBundle文件后缀名称
 		public const string StrBuildManifestFileName = "Manifest"; //构建输出的清单文件名称
 		public const string StrBuildPackageFileName = "package.bytes"; //构建输出的补丁文件名称
-
-
-		/// <summary>
-		/// 我们设定的打包根路径
-		/// </summary>
-		public const string StrMyPackRootPath = "Assets/Works/MyResources";
-
-		/// <summary>
-		/// 我们设定的图集根路径
-		/// </summary>
-		public const string StrMyUIAtlasFolderPath = "Assets/Works/MyResources/UIAtlas";
-
-		/// <summary>
-		/// 我们设定的精灵根路径（图集关联的精灵）
-		/// </summary>
-		public const string StrMyUISpriteFolderPath = "Assets/WorksArt/Panel/UISprite";
 	}
 }
