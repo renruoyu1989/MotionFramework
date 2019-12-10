61068e02857c4d2db2f4d367d637497f5dddd0e1
何冠峰 <hevinci@hotmail.com>
2019-12-10 10:50:44 +0800

    Update aduio manager

---
+++ Assets/MotionGame/Runtime/Game.Audio/AssetAudio.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using MotionEngine.Res;
+
+/// <summary>
+/// 音频资源类
+/// </summary>
+internal class AssetAudio : AssetObject
+{
+	/// <summary>
+	/// 标签
+	/// </summary>
+	public int AudioTag { private set; get; }
+
+	/// <summary>
+	/// 资源对象
+	/// </summary>
+	public AudioClip Clip { private set; get; }
+
+	public AssetAudio(int audioTag)
+	{
+		AudioTag = audioTag;
+	}
+	protected override bool OnPrepare(UnityEngine.Object mainAsset)
+	{
+		if (base.OnPrepare(mainAsset) == false)
+			return false;
+
+		Clip = mainAsset as AudioClip;
+		if (Clip == null)
+			return false;
+
+		return true;
+	}
+}
---
+++ Assets/MotionGame/Runtime/Game.Audio/AudioDefine.cs
@@ -0,0 +1,26 @@
+﻿
+/// <summary>
+/// 音频层级
+/// </summary>
+public enum EAudioLayer
+{
+	/// <summary>
+	/// 音乐
+	/// </summary>
+	Music,
+
+	/// <summary>
+	/// 环境
+	/// </summary>
+	Ambient,
+
+	/// <summary>
+	/// 语音
+	/// </summary>
+	Voice,
+
+	/// <summary>
+	/// 音效
+	/// </summary>
+	Sound,
+}
